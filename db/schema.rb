9c3c606f4a6f8ef6e40090d59d1828ecd85c38f6
Ed Tewiah <etewiah@hotmail.cim>
2016-12-15 19:38:00 +0100

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203130042) do
+ActiveRecord::Schema.define(version: 20161215152137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 20161203130042) do
     t.integer  "content_id"
     t.string   "image"
     t.string   "description"
+    t.string   "folder"
+    t.integer  "file_size"
     t.integer  "sort_order"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -163,6 +165,7 @@ ActiveRecord::Schema.define(version: 20161203130042) do
     t.string   "image"
     t.string   "description"
     t.string   "folder"
+    t.integer  "file_size"
     t.integer  "sort_order"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -265,6 +268,40 @@ ActiveRecord::Schema.define(version: 20161203130042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["link_key"], name: "index_pwb_sections_on_link_key", unique: true, using: :btree
+  end
+
+  create_table "pwb_users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,     null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "authentication_token"
+    t.boolean  "admin",                  default: false
+    t.string   "first_names"
+    t.string   "last_names"
+    t.string   "skype"
+    t.string   "phone_number_primary"
+    t.string   "default_client_locale"
+    t.string   "default_admin_locale"
+    t.string   "default_currency"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.index ["confirmation_token"], name: "index_pwb_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_pwb_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_pwb_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "translations", force: :cascade do |t|
