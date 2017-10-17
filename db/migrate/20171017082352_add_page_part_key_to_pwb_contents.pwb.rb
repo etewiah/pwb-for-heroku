6bebfa8c18ee4aa68bb58aa3c5b2038bf1d9f6de
Ed Tewiah <etewiah@hotmail.cim>
2017-10-17 10:27:26 +0200

    Update migrations for page_parts

--- db/migrate/20171017082352_add_page_part_key_to_pwb_contents.pwb.rb
+++ db/migrate/20171017082352_add_page_part_key_to_pwb_contents.pwb.rb
@@ -1,7 +1,7 @@
 # This migration comes from pwb (originally 20170917215533)
-class AddFragmentKeyToPwbContents < ActiveRecord::Migration[5.1]
+class AddPagePartKeyToPwbContents < ActiveRecord::Migration[5.1]
   def change
-    add_column :pwb_contents, :fragment_key, :string, index: true
+    add_column :pwb_contents, :page_part_key, :string, index: true
     # add_column :pwb_contents, :visible_on_page, :boolean, default: true
     # , index: true
   end
