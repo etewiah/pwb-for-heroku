b6c8af24fa3c570504c6a6fff54b677fa51cd00c
Ed Tewiah <etewiah@hotmail.cim>
2018-02-08 15:54:16 +0100

    Update PWB with admin assets for footer editing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,10 +37,6 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
@@ -51,4 +47,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = false
+  # TODO: - figure out why when above is set to false
+  # font and some image files do not get served
+  # http://stackoverflow.com/questions/19194515/rails-4-app-on-heroku-is-serving-css-and-js-but-not-image-assets?rq=1
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  # To test assets for heroku deploy, set below to false
+  config.assets.debug = true
 end
